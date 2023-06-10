96e07d4bc67e5dc3cac33fcceb19152271d9ea7a
difischer <difischer@uc.cl>
2023-06-10 16:29:24 -0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="IrisSegPy3",
+    version="0.1",
+    authors="Sandipan Banerjee,Domingo Mery, Diego Fischer",
+    author_email="difischer@uc.cl",
+    url="https://github.com/difischer/IrisSegPy3",
+    py_modules=["IrisSeg", "morphsnakes"],
+)
